cc9ac93ed999dc4d0a7d4e68fa9c2d61257d6cbd
Pay-Pulse Company <31178314+paypulse@users.noreply.github.com>
2023-08-07 06:41:23 +0900

    first Commit

---
+++ src/main/java/com/example/sampletalk/SampleTalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.sampletalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleTalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sampletalk/SampleTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampletalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
